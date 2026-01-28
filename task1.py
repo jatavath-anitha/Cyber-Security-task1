cc41eca5d45f4d2a42c7e4512e8b96544e14659b
jatavath-anitha <jatavathanitha32@gmail.com>
2026-01-28 21:57:49 +0530

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,8 +1,10 @@
 print("Hello, this is my Task 1")
+
 from scapy.all import sniff
 
 def packet_callback(packet):
     print(packet.summary())
 
 print("Starting network sniffer...")
+
 sniff(prn=packet_callback, count=10)
